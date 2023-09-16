a391a1210d90b7118b81aa7646164c1fe248ee0e
Nasirul Islam <nasirul.siat@outlook.com>
2023-09-17 02:49:50 +0600

    Add bean and create constructor for the Student class

---
+++ src/main/java/net/mfsiat/springbootrestapi/bean/Student.java
@@ -0,0 +1,13 @@
+package net.mfsiat.springbootrestapi.bean;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
