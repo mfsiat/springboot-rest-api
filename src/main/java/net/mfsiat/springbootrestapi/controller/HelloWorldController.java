0d9f3f4c00e6c38009ded2e46cba428c4a693d11
Nasirul Islam <nasirul.siat@outlook.com>
2023-09-17 02:42:43 +0600

    GetMapping ( http get request ) done

--- src/main/java/net/mfsiat/springbootrestapi/controller/HelloWorldController.java
+++ src/main/java/net/mfsiat/springbootrestapi/controller/HelloWorldController.java
@@ -1,7 +1,16 @@
 package net.mfsiat.springbootrestapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    //  Create the REST API
+    //  HTTP GET Request
+    //  http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWord(){
+        return "Hello World";
+    }
 }
