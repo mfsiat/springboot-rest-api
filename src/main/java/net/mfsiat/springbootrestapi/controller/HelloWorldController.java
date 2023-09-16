c17392a7c2ce2343ea85f4c4ff00720e5434946e
Nasirul Islam <nasirul.siat@outlook.com>
2023-09-17 02:37:36 +0600

    Add RestController annotation and removing the previous ones

--- src/main/java/net/mfsiat/springbootrestapi/controller/HelloWorldController.java
+++ src/main/java/net/mfsiat/springbootrestapi/controller/HelloWorldController.java
@@ -1,7 +1,7 @@
 package net.mfsiat.springbootrestapi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
 }
