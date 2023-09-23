083226b9cca01df5ae07eaecada613c3ca4180fc
Nasirul Islam <nasirul.siat@outlook.com>
2023-09-23 11:15:09 +0600

    Return a list of students

--- src/main/java/net/mfsiat/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/mfsiat/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.mfsiat.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     // Create the Spring Boot REST API
@@ -14,5 +17,21 @@ public class StudentController {
     public Student getStudent(){
         Student student = new Student(1, "Nasirul", "Islam");
         return student;
+    }
+
+    // Create the java method that returns the list of students
+    // After that make it a rest api using java annotations
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Nasirul", "Islam"));
+        students.add(new Student(2, "Wasif", "Shikdar"));
+        students.add(new Student(3, "Shiblur", "Rahman"));
+        students.add(new Student(4, "Maliha", "Shamshed"));
+        students.add(new Student(5, "Saiful", "Islam"));
+
+        // Return the object
+        return students;
     }
 }
